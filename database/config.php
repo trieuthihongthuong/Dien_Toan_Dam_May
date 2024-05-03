e881ec11348fb9e4826d83d04e1bc53bb6691bbc
trieuthihongthuong <156600026+trieuthihongthuong@users.noreply.github.com>
2024-05-03 22:30:46 +0700

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,5 +1,5 @@
 <?php
 define('HOST', 'us-cluster-east-01.k8s.cleardb.net');
-define('USERNAME', 'b249189585d978');
-define('PASSWORD', '7603f440');
-define('DATABASE', 'heroku_ea5583f614317c7');
+define('USERNAME', 'b209a43c122281');
+define('PASSWORD', '7929ed29');
+define('DATABASE', 'heroku_9918df2ff40cbf8');
